1cf69c9cf7a39eba3d8feaa1ed4bfd68fb8bd8e3
Ruby Weiner <rubyweiner796@me.com>
2018-06-14 20:11:51 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+  x = num1/num2
+  return x
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(argument)
+  argument
 end
 
-def greeting
-
+def greeting(greeting_message, name)
+  greeting_message
+  name
 end
 
 def return_a_value
-
+  phrase = "Nice"
 end
 
 def last_evaluated_value
-
+  phrase = "expert"
 end
 
-def pizza_party
-
+def pizza_party(phrase = "cheese")
+  phrase
 end
